2e00c556493366b977fcf3efffa4b16a0f4ec72a
Joseph Birr-Pixton <jpixton@gmail.com>
2015-07-08 18:52:25 +0100

    process results better

---
+++ pbkdf2/tests/results.py
@@ -0,0 +1,31 @@
+import sys, re
+from os import path
+
+export = dict(
+        openssl = 'openssl',
+        python = 'python-34',
+        nodejs = 'nodejs',
+        mono = 'mono',
+        superfast = 'superfast',
+        javaopenjdk = 'java-jdk',
+        golang = 'golang',
+        php = 'php5',
+        phppatch = 'php5-new'
+)
+
+if __name__ == '__main__':
+    tests = sys.argv[1:]
+    timings = {}
+
+    for dir in tests:
+        name = dir.replace('/', '')
+        tf = open(path.join(dir, 'timing')).read()
+        time = re.search(r'([0-9\.]+)user', tf).group(1)
+        timings[name] = time
+
+    print timings
+
+    with open('../measurements.tex', 'w') as f:
+        for name, test in export.items():
+            print >>f, '\def \%stime {%s}' % (name, timings[test])
+
